0f4e4b62fc69964b79c9b5874640dbb722282d41
Andrew W. Johnson <31744060+awjohnson@users.noreply.github.com>
2017-12-05 11:14:41 -0500

    Add files via upload

---
+++ Logout Scripts/sinc-ForceOut.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/bin/rm -Rf /Users/Shared/ForceOut/triger
+/usr/bin/killall loginwindow
+
+exit 0
+
---
+++ Logout Scripts/triger.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+/usr/bin/touch /Users/Shared/ForceOut/triger
+
+/bin/echo $0 > /private/tmp/triger.log
+
+exit 0
+
+
+
