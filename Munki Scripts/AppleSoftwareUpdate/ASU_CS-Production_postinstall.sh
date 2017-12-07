49973b621a0165699f2c1cd7c3cb5ae69815a0ae
Andrew W. Johnson <31744060+awjohnson@users.noreply.github.com>
2017-12-07 08:46:56 -0500

    Add files via upload

---
+++ Munki Scripts/AppleSoftwareUpdate/ASU_CS-Production_postinstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ASU_CS-Production_postinstall.sh
+
+#URL="http://asu.sinc.stonybrook.edu/index_testing.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_production.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_Servers.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_TLT-Staff.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_TV-Studio-Test.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_TV-Studio.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_CS-Testing.sucatalog"
+URL="http://asu.sinc.stonybrook.edu/index_CS-Production.sucatalog"
+
+/bin/echo "ASU_CS-Production: setting CatalogURL to: $URL"
+/usr/bin/defaults write /Library/Preferences/com.apple.SoftwareUpdate CatalogURL "$URL"
+
+exit 0
+
---
+++ Munki Scripts/AppleSoftwareUpdate/ASU_CS-Testing_installcheck.sh.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ASU_CS-Testing_installcheck.sh
+# this will run as a munki install_check script
+# exit status of 0 means install needs to run
+# exit status not 0 means no installation necessary
+
+
+#URL="http://asu.sinc.stonybrook.edu/index_testing.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_production.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_Servers.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_TLT-Staff.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_TV-Studio-Test.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_TV-Studio.sucatalog"
+URL="http://asu.sinc.stonybrook.edu/index_CS-Testing.sucatalog"
+#URL="http://asu.sinc.stonybrook.edu/index_CS-Production.sucatalog"
+
+CatalogURL=`/usr/bin/defaults read /Library/Preferences/com.apple.SoftwareUpdate CatalogURL`
+
+if [ $CatalogURL != $URL ]; then
+	/bin/echo "ASU_CS-Testing: CatalogURL for ASU not set correctly."
+	exit 0
+fi
+
+exit 1
+
