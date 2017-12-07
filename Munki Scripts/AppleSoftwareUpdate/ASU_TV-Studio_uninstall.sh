4dc7047160068befaaefba1515c85ac7dc26c6ef
Andrew W. Johnson <31744060+awjohnson@users.noreply.github.com>
2017-12-07 08:49:53 -0500

    Delete ASU_TV-Studio_uninstall.sh

--- Munki Scripts/AppleSoftwareUpdate/ASU_TV-Studio_uninstall.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-# ASU_Production_uninstall.sh
-
-/bin/echo "ASU_Production: Deleting CatalogURL."
-
-/usr/bin/defaults delete /Library/Preferences/com.apple.SoftwareUpdate CatalogURL
-
-exit 0
